93cd2af270c00490e7870a65c6043dbcb94573e0
Amrita Giri <amritagiri1610@gmail.com>
2021-03-24 20:22:58 +0000

    added contact form html

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,14 @@
 <!-- the head section -->
+
 <head>
-<title>My Strew Coffee website</title>
-<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+    <title>My Strew Coffee website</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyle.css">
+    <script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script>
 </head>
 
 <!-- the body section -->
+
 <body>
-<header><h1>STREW COFFEE SHOP</h1></header>
+    <header>
+        <h1>STREW COFFEE SHOP</h1>
+    </header>
