c6dfb601f0b78f596080a583214996c656c50850
Amrita Giri <amritagiri1610@gmail.com>
2021-03-25 01:09:51 +0000

    logout & icon added, scss accordingly adjusted

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,9 @@
             <a href="add_record_form.php"><i class="fa fa-refresh"></i> Add Coffee</a>
             <a href="category_list.php"><i class="fa fa-bars"></i> Manage Coffee</a>
             <a href="contact.php"><i class="fa fa-search"></i> Contact</a>
-            <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
             <a href="#"><i class="fa fa-male"></i> Register</a>
+            <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+            <a href="#"><i class="fa fa-close"></i> Logout</a>
+            
         </div>
     </header>
